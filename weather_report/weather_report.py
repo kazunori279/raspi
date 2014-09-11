ce20fd27a8e0f0e89026e59519952b15fb037a2a
Kazunori Sato <kazunori279@gmail.com>
2014-09-11 01:15:57 +0000

    Added weather_report.py

---
+++ weather_report/weather_report.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import subprocess
+import Adafruit_DHT
+
+DHT22_GPIO = 4
+LPS331_ADRS = "0x5d"
+
+def cmd_exec(cmd):
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    return stdout, stderr
+
+def i2cget(reg):
+    return cmd_exec("sudo i2cget -y 1 " + LPS331_ADRS + " " + reg)
+    
+def init_lps():
+
+    # WHO_AM_I
+    stdout, stderr = cmd_exec("sudo i2cget -y 1 " + LPS331_ADRS + " 0x0f")
+    if stderr != None:
+        return stdout, stderr
+    if stdout != "0xbb":
+        return stdout, "WHO_AM_I result mismatch."
+
+    # activate device
+    return cmd_exec("sudo i2cset -y 1 " + LPS331_ADRS + " 0x20 0x90")
+
+def read_lps():
+
+    # reading from LPS
+    out0, err0 = i2cget("0x28")
+    out1, err1 = i2cget("0x29")
+    out2, err2 = i2cget("0x2a")
+
+    # decoding the value
+    val = ((int(out0, 16) + int(out1, 16) * 0x100) + (int(out2, 16) * 0x10000)) / 4096
+    err = err0 + " " + err1 + " " + err2 
+    return val, err 
+    
+# init LPS     
+stdout, stderr = init_lps()
+if stderr != None and len(stderr.strip()) > 0:
+    print "LPS init error: " + stderr
+    exit() 
+
+# read LPS
+v, err = read_lps()
+print v
+ 
+h, t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, DHT22_GPIO)
+print h
+print t
+
