4dd1eeaddbb1558717a49daa7c0426b310ef0514
Kazunori Sato <kazunori279@gmail.com>
2014-09-12 13:45:54 +0000

    Fixed bugs for reading LPS, added exception handlings

--- weather_report/weather_report.py
+++ weather_report/weather_report.py
@@ -2,7 +2,7 @@
 #
 # Weather report: periodically reads temp, humidity and air pressure
 # from DHT22 and LPS331AP sensors connected to raspberry pi. Requires
-# Adafruite_DHT driver and i2ctools installed.
+# Adafruit_DHT driver and i2ctools installed.
 #
 # Usage: sudo python weather_report.py
 # 
@@ -16,7 +16,9 @@ LPS331_ADRS = "0x5d"
 def cmd_exec(cmd):
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = p.communicate()
-    return stdout, stderr
+    if stderr != None and len(stderr.strip()) > 0:
+        raise IOError("Error on executing cmd: " + stderr)
+    return stdout.strip()
 
 def i2cget(reg):
     return cmd_exec("i2cget -y 1 " + LPS331_ADRS + " " + reg)
@@ -24,35 +26,28 @@ def i2cget(reg):
 def init_lps():
 
     # WHO_AM_I
-    stdout, stderr = cmd_exec("i2cget -y 1 " + LPS331_ADRS + " 0x0f")
-    if stderr != None:
-        return stdout, stderr
+    stdout = cmd_exec("i2cget -y 1 " + LPS331_ADRS + " 0x0f")
     if stdout != "0xbb":
-        return stdout, "WHO_AM_I result mismatch."
+        raise IOError("WHO_AM_I result mismatch.")
 
     # activate device
-    return cmd_exec("i2cset -y 1 " + LPS331_ADRS + " 0x20 0x90")
+    cmd_exec("i2cset -y 1 " + LPS331_ADRS + " 0x20 0x90")
 
 def read_lps():
 
     # reading from LPS
-    out0, err0 = i2cget("0x28")
-    out1, err1 = i2cget("0x29")
-    out2, err2 = i2cget("0x2a")
+    out0 = i2cget("0x28")
+    out1 = i2cget("0x29")
+    out2 = i2cget("0x2a")
 
     # decoding the value
-    val = ((int(out0, 16) + int(out1, 16) * 0x100) + (int(out2, 16) * 0x10000)) / 4096
-    err = err0 + " " + err1 + " " + err2 
-    return val, err 
+    return (int(out0, 16) + (int(out1, 16) * 0x100) + (int(out2, 16) * 0x10000)) / 4096.0
     
 # init LPS     
-stdout, stderr = init_lps()
-if stderr != None and len(stderr.strip()) > 0:
-    print "LPS init error: " + stderr
-    exit() 
+init_lps()
 
 # read air pressure from LPS
-air_pressure, err = read_lps()
+air_pressure = read_lps()
 print air_pressure 
 
 # read humidity and temp from DHT 
