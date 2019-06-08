7d05592577809064380859e4a271a6cd036e34b8
Kaz Sato <kazunori279@gmail.com>
2019-06-09 00:01:38 +0900

    fixed layouts

--- regi/main.py
+++ regi/main.py
@@ -1,6 +1,6 @@
 """Self-service register demo"""
 
-import cv2, time
+import os, cv2, time
 import tkinter as tk 
 from tkinter.font import Font
 from edgetpu.classification.engine import ClassificationEngine
@@ -9,23 +9,26 @@ from PIL import Image, ImageTk
 # init Tk window
 tk_root = tk.Tk()
 tk_root.title('Self-service Register with RasPi + Edge TPU')
+tk_root.attributes("-fullscreen", True)
 tk_cam = tk.Canvas(tk_root, width=640, height=480)
-tk_cam.grid(row=0, column=0, rowspan=2, padx=10)
+tk_cam.grid(row=0, column=0, rowspan=2, padx=10, pady=50)
 tk_font = Font(family='Helvetica', size=18)
 
 # product buttons frame
-tk_buttons_frame = tk.Frame(tk_root, width=370, height=300)
+tk_buttons_frame = tk.Frame(tk_root, padx=20, pady=20, 
+  width=350, height=230)
 tk_buttons_frame.grid(row=0, column=1)
+tk_buttons_frame.propagate(False)
 
 # cart items frame
-tk_items_frame = tk.Frame(tk_root, width=370, height=180)
+tk_items_frame = tk.Frame(tk_root)
 tk_items_frame.grid(row=1, column=1)
-tk_items = tk.Listbox(tk_items_frame, height=3, font=tk_font)
+tk_items = tk.Listbox(tk_items_frame, height=5, font=tk_font)
 tk_items.insert(tk.END, 'test')
 tk_items.pack(side=tk.TOP)
 
 # checkout frame
-tk_cout_frame = tk.Frame(tk_items_frame, width=370, pady=10) 
+tk_cout_frame = tk.Frame(tk_items_frame, padx=20, pady=20) 
 tk_cout_frame.pack(side=tk.TOP)
 tk_total = tk.Label(tk_cout_frame, font=tk_font,
   text='Total: 1,000')
@@ -43,11 +46,27 @@ tpu = ClassificationEngine('/home/pi/model.tflite')
 # init labels
 labels = ['blouse','blouson','cardigan','check shirt','coat','color shirt','down coat','down jacket','down vest','dress','duffle coat','hoodie','jacket','jacket for ladies','knit vest','no sleeve shirt','pants','patterned shirt','polo shirt','school sailor','school suit','shirt','skirt','striped shirt','suit','suit for ladies','sweater','sweater highneck','tie','trench coat','tshirt','vest','wool jacket']
 
-# init label buttons
-label_buttons = {} 
+# load images
+label_images = {}
 for l in labels:
-  label_buttons[l] = tk.Button(tk_buttons_frame, font=tk_font, text=l)
-label_times = {}
+  p = 'img/' + l + '.png' 
+  if os.path.exists(p):
+    label_images[l] = ImageTk.PhotoImage(Image.open(p))
+    print('loaded: ' + p)
+blank_img = ImageTk.PhotoImage(Image.open('img/blank.png'))
+
+# init label buttons
+label_buttons = {}
+label_detected_times = {}
+for l in labels:
+  img = label_images[l] if l in label_images else blank_img 
+  fr = tk.Frame(tk_buttons_frame, padx=10) 
+  cvs = tk.Canvas(fr, width=119, height=100) 
+  cvs.pack(side=tk.LEFT)
+  cvs.create_image(0, 0, image=img, anchor='nw')
+  b = tk.Button(fr, font=tk_font, text=l, height=2, width=10)
+  b.pack(side=tk.LEFT)
+  label_buttons[l] = fr
 
 # main
 def main():
@@ -66,32 +85,28 @@ def main():
     tk_cam.create_image(0, 0, image=img_tk, anchor='nw')
 
     # classification with tpu
-    tpu_results = tpu.ClassifyWithImage(img_pil, top_k=3)
+    i, score = tpu.ClassifyWithImage(img_pil, top_k=1)[0]
+    label = labels[i]
+    print(label + ": " + str(score))
 
-    # update UI 
-    print('\n');
-    now = time.perf_counter()
-    for i, score in tpu_results:
+    # record a timestamp for the detected label
+    now = time.time()
+    if score > 0.4:
+      label_detected_times[label] = now
 
-      # print the result
-      label = labels[i]
-      print(label + ': ' + str(score))
-      if score < 0.5:
-        break
-
-      # show label button  
-      if not label_buttons[label].winfo_ismapped():
-        label_buttons[label].pack(side=tk.TOP)
-      label_times[label] = now
-
-    # hide older labels
+    # show or hide label buttons
+    count = 0
     for l in labels:
-      if l in label_times and (now - label_times[l] > 1):
+      is_visible = label_buttons[l].winfo_ismapped()
+      if is_visible:
+        count = count + 1
+      is_detected = now - label_detected_times[l] < 1.0 \
+        if l in label_detected_times else False
+      if count < 2 and not is_visible and is_detected: 
+        label_buttons[l].pack(side=tk.TOP)
+      elif is_visible and not is_detected: 
         label_buttons[l].pack_forget()
-
-#    time.sleep(1)
 
 if __name__ == '__main__':
   main()
-
 
